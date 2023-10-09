82423d9f951da4e4b4a1bc0ef62ae5dd73376f27
alizahedi <alizahedi499@gmail.com>
2023-10-09 22:26:44 +0330

    add urlpattern for meeting rooms count

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,18 +1,30 @@
 from django.urls import path, re_path
 
 from .views import (
-	signup, ProfileUpdateView, account_activation_sent_view, account_activate
+    signup,
+    ProfileUpdateView,
+    account_activation_sent_view,
+    account_activate,
+    set_meeting_rooms_count,
 )
 
-app_name = 'profiles'
+app_name = "profiles"
 
 urlpatterns = [
-	path('', signup, name='profile-signup'),
-	path('<int:pk>/edit/', ProfileUpdateView.as_view(), name='profile-edit'),
+    path("", signup, name="profile-signup"),
+    path("<int:pk>/edit/", ProfileUpdateView.as_view(), name="profile-edit"),
 ]
 
 urlpatterns += [
-	path('account-activation-sent/', account_activation_sent_view, name='account-activation-sent'),
-	path('activate/<uidb64>/<token>/',
-		account_activate, name='activate'),
+    path(
+        "account-activation-sent/",
+        account_activation_sent_view,
+        name="account-activation-sent",
+    ),
+    path("activate/<uidb64>/<token>/", account_activate, name="activate"),
+    path(
+        "set-meeting-rooms-count/",
+        set_meeting_rooms_count,
+        name="set_meeting_rooms_count",
+    ),
 ]
