ec762813de5c9e27dfbc8f3684cd29e5a79f4870
alizahedi <alizahedi499@gmail.com>
2023-10-09 22:29:34 +0330

    add a field for store number of rooms by managers

--- profiles/models.py
+++ profiles/models.py
@@ -1,19 +1,25 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class Profile(models.Model):
     USER_ROLES = (
-        ('normal', 'Normal'),
-        ('manager', 'Manager'),
+        ("normal", "Normal"),
+        ("manager", "Manager"),
     )
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=50, blank=True)
     location = models.CharField(max_length=30)
     email_confirmed = models.BooleanField(default=False)
-    role = models.CharField(max_length=10, choices=USER_ROLES, default='normal')
+    role = models.CharField(max_length=10, choices=USER_ROLES, default="normal")
+    meeting_rooms_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
 
 @receiver(post_save, sender=User)
 def update_user_profile(sender, instance, created, **kwargs):
