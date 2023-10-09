8cf85007328d0f9939e7878d31d04fb98e57ce92
alizahedi <alizahedi499@gmail.com>
2023-10-09 22:27:52 +0330

    add view of set meeting rooms count

--- profiles/views.py
+++ profiles/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate
-from django.views.generic import UpdateView
-from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, get_user_model, login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import redirect, render
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
-from django.contrib.auth import get_user_model
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.views.generic import UpdateView
 
-from .forms import SignUpForm
+from .forms import MeetingRoomsCountForm, SignUpForm
 from .models import Profile
-from .utils import get_location_from_ip
 from .tokens import account_activation_token
+from .utils import get_location_from_ip
 
 User = get_user_model()
 
+
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
@@ -25,7 +25,7 @@ def signup(request):
             user.save()
             user = form.save()
             user.refresh_from_db()  # load the profile instance created by the signal
-            user.profile.location = form.cleaned_data.get('location')
+            user.profile.location = form.cleaned_data.get("location")
             ip_address = user.profile.location
             location = get_location_from_ip(ip_address)
             # print(location)
@@ -33,24 +33,24 @@ def signup(request):
             user.save()
             current_site = get_current_site(request)
             subject = "Activate your Django Serives Account"
-            message = render_to_string('registration/account_activation_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
-                'token': account_activation_token.make_token(user)
-            })
+            message = render_to_string(
+                "registration/account_activation_email.html",
+                {
+                    "user": user,
+                    "domain": current_site.domain,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                    "token": account_activation_token.make_token(user),
+                },
+            )
             user.email_user(subject, message)
-            return redirect('profiles:account-activation-sent')
+            return redirect("profiles:account-activation-sent")
     else:
         form = SignUpForm()
-    return render(request, 'app/signup.html', {
-        'form': form,
-        'profile': True
-    })
+    return render(request, "app/signup.html", {"form": form, "profile": True})
 
 
 def account_activation_sent_view(request):
-    return render(request, 'registration/account_activation_sent.html')
+    return render(request, "registration/account_activation_sent.html")
 
 
 def account_activate(request, uidb64, token):
@@ -67,12 +67,28 @@ def account_activate(request, uidb64, token):
         user.profile.email_confirmed = True
         user.save()
         login(request, user)
-        return redirect('users:dashboard')
+        return redirect("users:dashboard")
     else:
-        return render(request, 'registration/account_activation_invalid.html')
+        return render(request, "registration/account_activation_invalid.html")
 
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = Profile
     template_name = "app/profile_edit.html"
-    fields = ('bio',)
+    fields = ("bio",)
+
+
+@login_required
+def set_meeting_rooms_count(request):
+    if request.user.profile.role == "manager":
+        if request.method == "POST":
+            form = MeetingRoomsCountForm(request.POST, instance=request.user.profile)
+            if form.is_valid():
+                form.save()
+                return redirect("users:dashboard")
+        else:
+            form = MeetingRoomsCountForm(instance=request.user.profile)
+
+        return render(request, "meetingroom/set_meeting_rooms_count.html", {"form": form})
+    else:
+        return redirect("users:dashboard")
